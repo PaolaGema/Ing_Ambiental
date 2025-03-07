c6b95b7abecce8ef7677607537b11f4143b522ce
Paola Gema <54041166+PaolaGema@users.noreply.github.com>
2025-03-07 17:05:17 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+// next.config.js
+const nextConfig = {
+    images: {
+      domains: ["serviciopagina.upea.bo"], // Agrega el dominio aquí
+    },
+  };
+  
+  module.exports = nextConfig;
+
+  
